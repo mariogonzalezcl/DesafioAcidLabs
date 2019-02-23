888f581ca9699fd39c8cfc895993a18298c4ff85
Mario Gonzalez <mario.gonzalez.aravena@gmail.com>
2019-02-23 15:11:11 -0300

    commit inicial, arquetipo gradle

---
+++ src/main/java/cl/mariogonzalez/desafioacidlabs/DesafioacidlabsApplication.java
@@ -0,0 +1,13 @@
+package cl.mariogonzalez.desafioacidlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioacidlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioacidlabsApplication.class, args);
+	}
+
+}
